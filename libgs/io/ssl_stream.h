a04e80c2704cb79f97ed93f1eaf15374616acf25
King <username_nullptr@163.com>
2024-05-12 14:24:21 +0800

    tmp

--- libgs/io/ssl_stream.h
+++ libgs/io/ssl_stream.h
@@ -29,6 +29,8 @@
 #ifndef LIBGS_IO_SSL_STREAM_H
 #define LIBGS_IO_SSL_STREAM_H
 
+#ifdef __ENABLE_SSL__
+
 #include <libgs/io/stream.h>
 
 namespace libgs::io
@@ -43,15 +45,56 @@ class LIBGS_CORE_TAPI basic_ssl_stream : public basic_stream<Exec>
 
 public:
 	using stream_type = Stream;
+	using stream_ptr = std::shared_ptr<stream_type>;
 	using executor_type = base_type::executor_type;
 
 public:
-	basic_ssl_stream(std::shared_ptr<stream_type> stream);
+	basic_ssl_stream(stream_ptr stream);
 	basic_ssl_stream(stream_type *stream);
 	~basic_ssl_stream() override = default;
 
 public:
+	[[nodiscard]] awaitable<size_t> read_data
+	(buffer<void*> buf, read_condition rc, cancellation_signal *cnl_sig, error_code &error) noexcept override;
+
+	[[nodiscard]] awaitable<size_t> write_data
+	(buffer<std::string_view> buf, cancellation_signal *cnl_sig, error_code &error) noexcept override;
+
+public:
+//	[[nodiscard]] const stream_ptr native_object() const;
+//	[[nodiscard]] stream_ptr native_object();
+
+protected:
+	stream_ptr m_stream;
 };
+
+template <concept_base_of_stream Stream, concept_execution Exec>
+basic_ssl_stream<Stream,Exec>::basic_ssl_stream(stream_ptr stream) :
+	m_stream(std::move(stream))
+{
+
+}
+
+template <concept_base_of_stream Stream, concept_execution Exec>
+basic_ssl_stream<Stream,Exec>::basic_ssl_stream(stream_type *stream) :
+	m_stream(stream)
+{
+
+}
+
+template <concept_base_of_stream Stream, concept_execution Exec>
+awaitable<size_t> basic_ssl_stream<Stream,Exec>::read_data
+(buffer<void*> buf, read_condition rc, cancellation_signal *cnl_sig, error_code &error) noexcept
+{
+
+}
+
+template <concept_base_of_stream Stream, concept_execution Exec>
+awaitable<size_t> basic_ssl_stream<Stream,Exec>::write_data
+(buffer<std::string_view> buf, cancellation_signal *cnl_sig, error_code &error) noexcept
+{
+
+}
 
 template <concept_base_of_stream Stream>
 using ssl_stream = basic_ssl_stream<Stream>;
@@ -63,6 +106,8 @@ template <concept_base_of_stream Stream>
 using ssl_stream_ptr = basic_stream_ptr<Stream>;
 
 } //namespace libgs::io
+//#include <libgs/io/detail/ssl_stream.h>
 
+#endif //__ENABLE_SSL__
 
 #endif //LIBGS_IO_SSL_STREAM_H
